efd8a42bef3b3538557db1c2c4530420cdfc22b8
Yash Lad <lad.yas@northeastern.edu>
2025-03-16 20:06:27 -0400

    copy command for calendar logic added

--- src/controller/command/CalendarCommandFactory.java
+++ src/controller/command/CalendarCommandFactory.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import model.calendar.CalendarManager;
+import model.exceptions.DuplicateCalendarException;
+import model.exceptions.InvalidTimezoneException;
 import view.ICalendarView;
 
 public class CalendarCommandFactory {
@@ -35,15 +37,43 @@ public class CalendarCommandFactory {
     commands.put("copy", this::executeCopyCommand);
   }
 
-  private String executeCopyCommand(String[] strings) {
+  private String executeCopyCommand(String[] args) throws DuplicateCalendarException,
+          InvalidTimezoneException {
+
+    if(args.length<4) {
+      return "Error: Insufficient arguments for create calendar command";
+    }
+
+    if (!args[0].equals("calendar")) {
+      return "Error: Expected 'calendar' argument";
+    }
+
+    if (!args[1].equals("--name")) {
+      return "Error: Expected '--name' flag";
+    }
+
+    String calendarName = args[2];
+
+    if (!args[3].equals("--timezone")) {
+      return "Error: Expected '--timezone' flag";
+    }
+
+    String timezone = args.length > 4 ? args[4] : "America/New_York";
+
+    calendarManager.createCalendar(calendarName, timezone);
+
+    return "Calendar '"+calendarName+"' created with timezone '"+timezone+"'";
   }
 
   private String executeUseCalendarCommand(String[] strings) {
+    return "";
   }
 
   private String executeEditCalendarCommand(String[] strings) {
+    return "";
   }
 
   private String executeCreateCalendarCommand(String[] strings) {
+    return "";
   }
 }
